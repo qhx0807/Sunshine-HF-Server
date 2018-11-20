cfe8bfa50bcd7c8a53c659c61865503c28694503
qhx0807 <820240134@qq.com>
2018-11-20 14:10:24 +0800

    sms

--- routes/sms.js
+++ routes/sms.js
@@ -28,4 +28,22 @@ router.get('/verifycode', function (req, res, next) {
   })
 })
 
+router.get('/logincode', function (req, res, next) {
+  var code = creatRandomNum()
+  var password = md5(config.smsId + md5(config.smsSecret))
+  var username = req.query.name || ''
+  var mobile = config.adminPhone
+  var tmp = `【阳光宏帆】：本次后台登录验证码：${code}，10分钟内有效。登录用户：${username}`
+  var content = encodeURI(tmp)
+  var url = config.smsUrl + '?username=' + config.smsId + '&password=' + password + '&mobile=' + mobile + '&content=' + content
+  request(url, function (error, response, body) {
+    if(error){
+      res.json({ Data: error })
+    } else {
+      var expression = new Date().valueOf() + 10*60*1000
+      res.json({ code: code, sms: response.body, expression: expression })
+    }
+  })
+})
+
 module.exports = router;
